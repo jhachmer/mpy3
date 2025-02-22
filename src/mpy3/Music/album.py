d11ffa79119a2208f612e722254acdd22f5d961a
Jannes <jannes.hachmer@gmail.com>
2025-02-22 05:19:47 +0100

    added str and repr methods to album and track classes as well as classmethod to construct a Track Object from an IDv3 tag

--- src/mpy3/Music/album.py
+++ src/mpy3/Music/album.py
@@ -1,22 +1,46 @@
 from dataclasses import dataclass
-from datetime import datetime
 
 
 @dataclass
-class Track(object):
+class Track:
     title: str
     artist: str
     album: str
-    year: datetime
+    year: int
     genre: str
     track_number: int
 
+    def __repr__(self) -> str:
+        return f"{self.artist} - {self.title}"
+
+    def __str__(self) -> str:
+        return f"({self.track_number}) - {self.title}"
+
+    @classmethod
+    def from_id3v1(cls, id3v1, track_number):
+        return cls(
+            id3v1.title,
+            id3v1.artist,
+            id3v1.album,
+            id3v1.year,
+            id3v1.genre,
+            track_number,
+        )
+
 
 @dataclass
-class Album(object):
+class Album:
     title: str
     artist: str
-    year: datetime
-    genre: str
+    year: int
     tracks: list[Track]
     total_tracks: int
+
+    def __repr__(self) -> str:
+        return f"{self.artist} - {self.title} ({self.year})"
+
+    def __str__(self) -> str:
+        return (
+            f"{self.artist} - {self.title} ({self.year})\n"
+            f"{'\n'.join([str(track) for track in self.tracks])}"
+        )
