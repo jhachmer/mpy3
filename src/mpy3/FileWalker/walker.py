d98f79ff13ad03bdaf6b9215d8864ae127cdddfe
Jannes <jannes.hachmer@gmail.com>
2025-02-22 05:22:19 +0100

    Walker class method walk yields Album objects of every subdirectory of given root directory
    currently all fileformats except mp3 are ignored

--- src/mpy3/FileWalker/walker.py
+++ src/mpy3/FileWalker/walker.py
@@ -1,5 +1,8 @@
+import logging
 import os
-from mpy3.ID3V1.id3v1 import ID3V1Tag, ID3V1TagError
+
+from mpy3.IDV3.ID3V1.id3v1 import ID3V1Tag, ID3V1TagError
+from mpy3.Music.album import Album, Track
 
 
 class Walker:
@@ -7,16 +10,36 @@ class Walker:
         self.path = path
 
     def walk(self):
+        # albums = []
+        prev_dir = None
         for root, dirs, files in os.walk(self.path):
-            for file in files:
-                _, filext = os.path.splitext(file)
-                if filext == ".mp3":
-                    try:
-                        yield ID3V1Tag(os.path.join(root, file))
-                    except ID3V1TagError as iderror:
-                        print(iderror)
-                        continue
-                    except Exception as e:
-                        print(e)
-            # for dir in dirs:
-            #     yield os.path.join(root, dir)
+            if prev_dir is None and dirs == []:
+                prev_dir = root
+
+            if prev_dir != root:
+                tracks = []
+                for file in files:
+                    _, filext = os.path.splitext(file)
+                    if filext == ".mp3":
+                        try:
+                            track = Track.from_id3v1(
+                                ID3V1Tag(os.path.join(root, file)), len(tracks) + 1
+                            )
+                            tracks.append(track)
+                        except ID3V1TagError as iderror:
+                            logging.info(iderror)
+                            continue
+                        except Exception as e:
+                            logging.error(e)
+                            continue
+                if len(tracks) > 0:
+                    album = Album(
+                        tracks[0].album,
+                        tracks[0].artist,
+                        tracks[0].year,
+                        tracks,
+                        len(tracks),
+                    )
+                    # albums.append(album)
+                    yield album
+        # return albums
