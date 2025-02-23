723bb1207aecdc1470dd56ec8a7e21f333b50c91
Jannes <jannes.hachmer@gmail.com>
2025-02-23 22:05:58 +0100

    reworked file walker

--- src/mpy3/FileWalker/walker.py
+++ src/mpy3/FileWalker/walker.py
@@ -1,45 +1,54 @@
 import logging
 import os
 
-from mpy3.IDV3.ID3V1.id3v1 import ID3V1Tag, ID3V1TagError
+from mpy3.ID3.ID3V1.id3v1 import ID3V1Tag, ID3V1TagError
 from mpy3.Music.album import Album, Track
 
 
+class MusicTree(object):
+    def __init__(self) -> None:
+        self.map: dict[str, dict[str, Album]] = {}
+
+    def add_track(self, track: Track) -> None:
+        self.map[track.artist][track.album].add_track_to_album(track)
+
+    def add_album(self, album: Album) -> None:
+        self.map[album.artist][album.title] = album
+
+    def check_album(self, artist_name: str, album_name: str) -> bool:
+        return album_name in self.map[artist_name]
+
+    def check_artist(self, artist_name: str) -> bool:
+        return artist_name in self.map
+
+
 class Walker:
-    def __init__(self, path):
+    def __init__(self, path: str) -> None:
         self.path = path
 
-    def walk(self):
-        # albums = []
-        prev_dir = None
-        for root, dirs, files in os.walk(self.path):
-            if prev_dir is None and dirs == []:
-                prev_dir = root
-
-            if prev_dir != root:
-                tracks = []
-                for file in files:
-                    _, filext = os.path.splitext(file)
-                    if filext == ".mp3":
-                        try:
-                            track = Track.from_id3v1(
-                                ID3V1Tag(os.path.join(root, file)), len(tracks) + 1
-                            )
-                            tracks.append(track)
-                        except ID3V1TagError as iderror:
-                            logging.info(iderror)
-                            continue
-                        except Exception as e:
-                            logging.error(e)
-                            continue
-                if len(tracks) > 0:
-                    album = Album(
-                        tracks[0].album,
-                        tracks[0].artist,
-                        tracks[0].year,
-                        tracks,
-                        len(tracks),
+    def walk(self) -> MusicTree:
+        tree = MusicTree()
+        for root, _, files in os.walk(self.path):
+            for file in files:
+                # _, filext = os.path.splitext(file)
+                # if filext == ".mp3":
+                try:
+                    track = Track.from_id3v1(ID3V1Tag(os.path.join(root, file)))
+                    if not tree.check_artist(track.artist):
+                        tree.map[track.artist] = {}
+                    if not tree.check_album(track.artist, track.album):
+                        tree.add_album(
+                            Album(track.album, track.artist, track.year, [], 0)
+                        )
+                    track.track_number = (
+                        len(tree.map[track.artist][track.album].tracks) + 1
                     )
-                    # albums.append(album)
-                    yield album
-        # return albums
+                    tree.add_track(track)
+                except ID3V1TagError as iderror:
+                    logging.error(iderror)
+                    continue
+                # except Exception as e:
+                #     logging.error(f"blubb {e}")
+                #     continue
+
+        return tree
